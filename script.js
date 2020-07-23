203ec97148bbe6c322b111337de8f1a0b51e1cb9
K'Shelle Waller <kshellew@gmail.com>
2020-07-23 18:00:32 -0400

    responses now display in the DOM

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
+    $('.intro').addClass('hidden');
     getDinner();
     getDrink();
   });
@@ -10,14 +11,41 @@ function watchForm() {
 
 function getDinner() {
     fetch('https://api.spoonacular.com/recipes/random?number=3&tags=dinner&apiKey=3189deaed56f4d3fbcff0482cb212351')
-      .then(response => response.json())
-      .then(responseJson => console.log(responseJson));
+      .then(responseDinner => responseDinner.json())
+      .then(responseDinnerJson => displayMeal(responseDinnerJson)
+      .catch(err=> {
+        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      })
+      );
   }
 function getDrink() {
     fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-        .then(response => response.json())
-        .then(responseJson => console.log(responseJson));
+        .then(responseDrink => responseDrink.json())
+        .then(responseDrinkJson => displayDrink(responseDrinkJson))
+        .catch(err =>{
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
+} 
+
+function displayMeal(responseDinnerJson) {
+    //clears the result and appends the returned meal
+    $('.js-dinner-results')
+        .html('')
+        .append(`this is a meal test`);
+    
+        //display the results section
+    $('.dinner-results').removeClass('hidden');
 }
+
+function displayDrink(responseDrinkJson) {
+     //clears the result and appends the returned meal
+     $('.js-drink-results')
+     .html('')
+     .append(`this is a drink test`);
+ 
+     //display the results section
+ $('.drink-results').removeClass('hidden');
+} 
 
 $(function() {
   console.log('App loaded! Waiting for submit!');
